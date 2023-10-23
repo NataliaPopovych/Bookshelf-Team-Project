b1886d517092225b40d6962b0ed5155c5c4eadeb
Natalia Popovych <n.gumench@gmail.com>
2023-10-23 17:41:14 +0300

    changes

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import './js/components/header.js';
+import './js/components/authorization.js';
+import "./js/components/category.js";
+import './js/components/slider.js';
+import './js/components/home-page'
+import './js/components/switchThems';
+
+
+import './js/about-book-modal';
+import './js/modals';
+
+import './js/components/scrollUp.js';
